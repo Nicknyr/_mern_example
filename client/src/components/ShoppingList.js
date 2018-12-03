b0157872cdba1571b2fcba7bb4c1c3be38342c0d
Nick <nick_kinlen@hotmail.com>
2018-12-03 16:22:39 -0500

    At end of video 5. Basic Redux set up done.

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,28 +2,21 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { getItems } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            items: [
-                // uuid generates random ids for the API
-                { id: uuid(), name: "Eggs" },
-                { id: uuid(), name: "Milk" },
-                { id: uuid(), name: "Steak" },
-                { id: uuid(), name: "Water" }
-            ]
-        };
+    componentDidMount() {
+        this.props.getItems();
     }
-
 
     render() {
         // Uses destructuring to get this.state.items and just store them in {items}
-        const { items } = this.state;
-        console.log({items});
-
+        const { items } = this.props.item;
+        
+    
         return(
             <Container>
               <Button 
@@ -69,4 +62,13 @@ class ShoppingList extends Component {
 
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    item: state.item
+})
+
+export default connect(mapStateToProps, { getItems })(ShoppingList);
